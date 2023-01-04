9e60ca0d2623b1a360f5dc4296996ce3822c530c
Jacob Pantuso <jacobpantuso@outlook.com>
2023-01-03 19:35:49 -0500

    fixed a bug where selection box broke validation

--- main.js
+++ main.js
@@ -2,11 +2,10 @@
 function checkContact() {
     var name = document.getElementById("name").value;
     var problem = document.getElementById("problem").value;
-    var problem_type = document.getElementById("problem_type").value;
+    var type = document.getElementById("mySelect").value;
     var submit = document.getElementById("submit");
-    if (name != "" && problem_type != "Select a Problem" && problem != "") {
+    if (name != "" && type != "select" && problem != "") {
         submit.style.backgroundColor = "#4CAF50";
         submit.style.pointerEvents = "all";
-        console.log("green");
     }
 }
