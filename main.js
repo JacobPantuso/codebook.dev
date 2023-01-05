2ade209f382374836c49d9ff5140d5fe748e67ba
Jacob Pantuso <jacobpantuso@outlook.com>
2023-01-04 20:11:37 -0500

    finalized about page

--- main.js
+++ main.js
@@ -9,3 +9,19 @@ function checkContact() {
         submit.style.pointerEvents = "all";
     }
 }
+
+function checkSubmit() {
+    var name = document.getElementById("name").value;
+    var submit = document.getElementById("submit");
+    if (name != "") {
+        submit.style.backgroundColor = "#4CAF50";
+        submit.style.pointerEvents = "all";
+    } else {
+        submit.style.backgroundColor = "red";
+        submit.style.pointerEvents = "none";
+    }
+}
+
+if (window.location.href.match('wip.html') != null) {
+    console.log("all good");
+   }
