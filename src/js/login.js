384e49f1907f55b6cc7e02610899eb1108cfd37d
Jacob Pantuso <jacobpantuso@outlook.com>
2023-01-14 15:07:07 -0500

    Adding redirects for new and existing users

--- src/js/login.js
+++ src/js/login.js
@@ -1,4 +1,4 @@
-function validateInput() {
+/*function validateInput() {
   // Get the input values
   event.preventDefault();
   var email = document.getElementById("email").value;
@@ -11,4 +11,21 @@ function validateInput() {
     console.log("Password is too short");
     document.getElementById("password").style.boxShadow = "0px 0px 30px 0px rgb(255, 0, 0)";
   }
-}
+} */
+
+// If the user is logged in and is not a new user redirect to code.html (using netlify redirect)
+netlifyIdentity.on("login", user => {
+  if (user.app_metadata.newUser === false) {
+    window.location.href = "/code.html";
+  }
+});
+
+netlifyIdentity.on("logout", () => {
+  window.location.href = "/index.html";
+});
+
+netlifyIdentity.on("login", user => {
+  if (user.app_metadata.newUser === true) {
+    window.location.href = "/get-started.html";
+  }
+});
